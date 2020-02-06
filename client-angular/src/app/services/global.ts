3e0113391420ef42b227b7fcdd58d484fe4cbbc6
djuanguillermo <djuanguillermo@gmail.com>
2020-02-06 10:42:49 -0500

    api global

--- client-angular/src/app/services/global.ts
+++ client-angular/src/app/services/global.ts
@@ -1,5 +1,5 @@
 export var GLOBAL = {
-	url: 'http://apilaravel.com.devel/'+'api/'
-	//url: 'http://apilaravel.tumecontratas.delcastic.com/'+'api/'
+	//url: 'http://apilaravel.com.devel/'+'api/'
+	url: 'http://apilaravel.tumecontratas.delcastic.com/'+'api/'
 	
 };
