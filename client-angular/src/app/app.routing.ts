18023c17a3530c86e6463f3aac672f79acdd5661
djuanguillermo <djuanguillermo@gmail.com>
2020-06-24 12:08:16 -0500

    first routing by seccion success

--- client-angular/src/app/app.routing.ts
+++ client-angular/src/app/app.routing.ts
@@ -8,6 +8,9 @@ import { DefaultComponent } from './components/default/default.component';
 
 import { MultiservicesDropdownComponent } from './components/multiservices-dropdown/multiservices-dropdown.component';
 import { SerachIndexComponent } from './components/serach-index/serach-index.component';
+import { CarouselIndexComponent } from './components/carousel-index/carousel-index.component';
+
+
 
 import { CarNewComponent } from './components/car-new/car-new.component';
 import { CarEditComponent } from './components/car-edit/car-edit.component';
@@ -15,22 +18,27 @@ import { CarDetailComponent } from './components/car-detail/car-detail.component
 
 const appRoutes: Routes = [
 	//{path:'', component: DefaultComponent},
-
-	{path:'', component: MultiservicesDropdownComponent},
-	{path:'serach-index', component: SerachIndexComponent},
+	//	{path:'', component: MultiservicesDropdownComponent},
+	{
+		path: '', children: [
+			{ path: '', outlet: 'seccion1', component: CarouselIndexComponent },
+			{ path: '', outlet: 'seccion2', component: MultiservicesDropdownComponent },
+		]
+	},
+	{ path: 'serach-index', component: SerachIndexComponent },
 	//{path:'inicio', component: DefaultComponent},
-	{path:'login', component: LoginComponent},
-	{path:'logout/:sure', component: LoginComponent},
-	{path:'registro', component: RegisterComponent},
+	{ path: 'login', component: LoginComponent },
+	{ path: 'logout/:sure', component: LoginComponent },
+	{ path: 'registro', component: RegisterComponent },
 	//{path:'inicio', component: MultiservicesDropdownComponent},
 
-	
-	{path:'crear-coche', component: CarNewComponent},
-	{path:'editar-coche/:id', component: CarEditComponent},
-	{path:'coche/:id', component: CarDetailComponent},
-	
-//	{path:'**', component: DefaultComponent},
-	{path:'**', component: SerachIndexComponent}
+
+	{ path: 'crear-coche', component: CarNewComponent },
+	{ path: 'editar-coche/:id', component: CarEditComponent },
+	{ path: 'coche/:id', component: CarDetailComponent },
+
+	//	{path:'**', component: DefaultComponent},
+	{ path: '**', component: SerachIndexComponent }
 ];
 
 export const appRoutingProviders: any[] = [];
